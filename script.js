65dae7d2fb03e7b0acdf508898609062732187c0
RoninsSeal <84649268+RoninsSeal@users.noreply.github.com>
2022-09-12 23:37:12 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+/*
+GOALS
+- Database
+- Move Pointer on Map
+- GPS Tracking
+*/
+
+// import {System} from "Firebase/system.js"
+// import {update, ref, onValue} from 'https://www.gstatic.com/firebasejs/9.8.1/firebase-database.js';
